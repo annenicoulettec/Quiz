8db8dfea89da2de27923af4fb3ead3ec7546d298
unknown <annenicoulettecabrera@yahoo.com>
2015-03-25 21:48:21 +0800

    Initial commit

---
+++ MvcApplication1/App_Start/AuthConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Web.WebPages.OAuth;
+using MvcApplication1.Models;
+
+namespace MvcApplication1
+{
+    public static class AuthConfig
+    {
+        public static void RegisterAuth()
+        {
+            // To let users of this site log in using their accounts from other sites such as Microsoft, Facebook, and Twitter,
+            // you must update this site. For more information visit http://go.microsoft.com/fwlink/?LinkID=252166
+
+            //OAuthWebSecurity.RegisterMicrosoftClient(
+            //    clientId: "",
+            //    clientSecret: "");
+
+            OAuthWebSecurity.RegisterTwitterClient(
+            consumerKey: "ptqocfQaiJKB7FsjWzMuZx3TU",
+            consumerSecret: "tHhLT9A0kq5BWjBBEyo8Qmug8nkln5oziuBH16TzIBQg5tvjXp");
+
+            OAuthWebSecurity.RegisterFacebookClient(
+            appId: "1603214249915925",
+            appSecret: "3601ed75b63e075af4337e8b325e921c");
+
+            //OAuthWebSecurity.RegisterGoogleClient();
+        }
+    }
+}
---
+++ MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication1.Models;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+        [Authorize(Roles = "Admin")]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+        [Authorize]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            var context = new UsersContext();
+            var username = User.Identity.Name;
+            var user = context.UserProfiles.SingleOrDefault(u => u.UserName == username);
+            ViewBag.Email = user.Email;
+
+            return View();
+        }
+    }
+}
